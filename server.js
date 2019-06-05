0e3ea2f67fb1862574a314d22cf17ac7e53237ae
Akinolae <makindeakinola22@gmail.com>
2019-06-05 17:08:32 +0100

    new addition

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/register', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'register.html'))
+})
+
+app.get('/logIn', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`server started at ${PORT}`));
